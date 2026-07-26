0c4ecd98b8c8ff8363c28e45121674982a98bf9f
LJY20070105 <3208140899@qq.com>
2026-07-26 17:57:18 +0800

    上传复数类 C++ 代码

--- 源.cpp
+++ 源.cpp
@@ -1,44 +1,41 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main()
-{
-	int a[50], m, x=0, b = 0, c = 1, mid, i, n;
-	cout << "how many numbers?";
-	cin >> n;
-	cout << endl;
-	cout << "how many duan";
-	cin >> m;
-	cout << endl;
-	cout << "enter numbers";
-	for (i = 0; i < n; i++)
-		cin >> a[i];
-	cout << endl;
-	int left = a[n-1], right = 0;
-	for (i = 0; i < n; i++)
-		right = right + a[i];
-	
-	while (left <= right)
-	{
-		b = 0; c = 1;
-		mid=(left + right) / 2;
-		for (i = 0; i < n; i++)
-		{
-			b = b + a[i];
-			if (b > mid)
-			{
-				c++;
-				b = a[i];
-			}
-		}
-		if (c <= m)
-		{
-			right = mid-1;
-			x = mid;
-		}
-		else
-			left = mid + 1;
+class complex {
+private:
+	double re;
+	double im;
+public:
+	complex(double r=0,double i=0):re(r),im(i){}
+	double real() const {
+		return re;
 	}
-	cout << x << endl;
+	double imag() const {
+		return im;
+	}
+	complex& operator +=(const complex& c) {
+		re += c.real();
+		im += c.imag();
+		return *this;
+	}
+	friend std::ostream& operator <<(ostream& os, const complex& c) {
+		return os<<"("<<c.real()<<","<<c.imag()<<")";
+	}
+	
+
+
+};
+complex operator +(const complex& c1, const complex& c2) {
+	return complex(c1.real() + c2.real(), c1.imag() + c2.imag());
+}
+int main (){
+	complex c1(1,2),c2(3,4),c3;
+	c3 = c1 + c2;
+	c1 += c2;
+	cout<<"c3="<<c3<<endl;
+	cout <<c1 << endl;
 	return 0;
 
+
+
+    
 }
